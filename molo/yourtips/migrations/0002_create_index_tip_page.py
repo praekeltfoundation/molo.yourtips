5c17885329f4fcdfd66b3f6ded1187c3c90b9746
cblignaut <cilliers@praekeltconsulting.com>
2017-07-10 13:56:33 +0200

    Fix flake8 errors

--- molo/yourtips/migrations/0002_create_index_tip_page.py
+++ molo/yourtips/migrations/0002_create_index_tip_page.py
@@ -15,7 +15,9 @@ def create_yourtips_index(apps, schema_editor):
         tip_index = YourTipsIndexPage(title='Your Tips', slug='your-tips')
         main.add_child(instance=tip_index)
         tip_index.save_revision().publish()
-        tip_section_index = YourTipsSectionIndexPage(title='Converted Tips', slug='converted-tips')
+        tip_section_index = YourTipsSectionIndexPage(
+            title='Converted Tips', slug='converted-tips'
+        )
         main.add_child(instance=tip_section_index)
         tip_section_index.save_revision().publish()
 

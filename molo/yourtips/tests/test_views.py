a36832ec404fda56b14980bf5619d6727fcc211c
cblignaut <cilliers@praekeltconsulting.com>
2017-07-10 11:22:11 +0200

    PR changes, added popular tip test

--- molo/yourtips/tests/test_views.py
+++ molo/yourtips/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
 
 from molo.yourtips.tests.base import BaseYourTipsTestCase
 from molo.yourtips.models import (
@@ -49,5 +50,28 @@ class TestYourTipsViewsTestCase(BaseYourTipsTestCase):
         article.save_revision().publish()
 
         response = self.client.get(reverse('molo.yourtips:recent_tips'))
+        self.assertContains(response, 'Test')
+        self.assertContains(response, 'test body')
+
+    def test_yourtips_popular_tip_view(self):
+        self.client.login(
+            username=self.superuser_name,
+            password=self.superuser_password
+        )
+
+        entry = YourTipsEntry.objects.create(
+            optional_name='Test',
+            tip_text='test body',
+            allow_share_on_social_media=True,
+        )
+
+        self.client.get(
+            '/django-admin/yourtips/yourtipsentry/%d/convert/' % entry.id
+        )
+        article = YourTipsArticlePage.objects.get(title='Tip-%s' % entry.id)
+        article.add_vote('1.2.3.4', 1)
+        article.save_revision().publish()
+
+        response = self.client.get(reverse('molo.yourtips:popular_tips'))
         self.assertContains(response, 'Test')
         self.assertContains(response, 'test body')

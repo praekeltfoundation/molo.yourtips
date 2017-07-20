a775454206486ad4bb13ff8d40d7144bd3c85761
cblignaut <cilliers@praekeltconsulting.com>
2017-07-20 14:56:53 +0200

    MOLO-493 Word and character count

--- molo/yourtips/forms.py
+++ molo/yourtips/forms.py
@@ -10,6 +10,24 @@ class YourTipsEntryForm(forms.ModelForm):
     tip_text = forms.CharField(widget=forms.Textarea(
     ))
 
+    def clean_tip_text(self):
+        data = self.cleaned_data['tip_text']
+        if len(data.split()) > 30:
+            raise forms.ValidationError(
+                "Sorry your tip is too long, "
+                "please edit it and cut down %d words." % (
+                    len(data.split()) - 30
+                )
+            )
+        elif len(data) > 140:
+            raise forms.ValidationError(
+                "Sorry your tip is too long, "
+                "please edit it and cut down %d characters." % (
+                    len(data) - 140
+                )
+            )
+        return data
+
     class Meta:
         model = YourTipsEntry
         fields = [

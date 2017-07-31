48c04e2ccebdf0b7f9aac02bfe1e222a3fcab83e
cblignaut <cilliers@praekeltconsulting.com>
2017-07-31 10:12:33 +0200

    Fixed tests

--- molo/yourtips/tests/test_models.py
+++ molo/yourtips/tests/test_models.py
@@ -61,3 +61,30 @@ class TestYourTipsModels(BaseYourTipsTestCase, MoloTestCaseMixin):
         self.assertEquals(YourTip.get_number_of_popular_tips(), 0)
         article.add_vote('1.2.3.4', 1)
         self.assertEquals(YourTip.get_number_of_popular_tips(), 1)
+
+    def test_share_increase(self):
+        self.client.login(
+            username=self.superuser_name,
+            password=self.superuser_password
+        )
+
+        tip_page = YourTip(
+            title='Test Tip',
+            description='This is the description',
+            slug='test-tip')
+        self.tip_index.add_child(instance=tip_page)
+        tip_page.save_revision().publish()
+
+        entry = YourTipsEntry.objects.create(
+            optional_name='Test',
+            tip_text='test body',
+            allow_share_on_social_media=False,
+        )
+        self.client.get(
+            '/django-admin/yourtips/yourtipsentry/%d/convert/' % entry.id
+        )
+        article = YourTipsArticlePage.objects.get(title='Tip-%s' % entry.id)
+        article.save_revision().publish()
+        self.assertEquals(article.number_of_shares, 0)
+        article.share_increase()
+        self.assertEquals(article.number_of_shares, 1)
